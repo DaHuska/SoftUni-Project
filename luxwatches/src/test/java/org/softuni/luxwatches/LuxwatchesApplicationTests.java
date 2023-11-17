d7317ca99a98c26d137a0fe92e426566f4d250f9
DaHuska <aboyanov@abv.bg>
2023-11-17 21:56:31 +0200

    Initial commit

---
+++ luxwatches/src/test/java/org/softuni/luxwatches/LuxwatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softuni.luxwatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxwatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
